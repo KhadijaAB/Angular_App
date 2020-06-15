62e1e189e403f18048144a529c2d99ce2c60b813
Khadija AB <Khadijaabeeb@hotmail.com>
2020-06-15 03:01:28 +0300

    ToDoListApp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'firstapp';
+  title = 'ToDo List App';
+  tasks= ['Reading', 'Practicing'];
+
+  addtask(newtask){
+  if(newtask==""){
+    alert("You Must Write Something!!");}
+   else{
+    this.tasks.push(newtask);
+   }
+  }
+
+  removetask(task){
+  let i=this.tasks.lastIndexOf(task);
+  this.tasks.splice(i,1);
+  }
+
 }
